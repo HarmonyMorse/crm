faef9db5da8bec8d52da1b5d9e065868c37de7be
HarmonyMorse <harmonythedev@gmail.com>
2025-01-20 17:45:53 -0800

    Get config environments complete

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+export default {
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier'
+    ],
+    ignorePatterns: ['dist', '.eslintrc.js'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['react-refresh'],
+    rules: {
+        'react-refresh/only-export-components': [
+            'warn',
+            { allowConstantExport: true },
+        ],
+    },
+    settings: {
+        react: {
+            version: 'detect'
+        }
+    }
+} 
